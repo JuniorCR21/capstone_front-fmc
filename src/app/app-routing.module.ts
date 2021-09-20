2ef171c596518c87fcb1d032a487ee4e4210a7a5
Junior Carrion <juniorcr2198@gmail.com>
2021-09-20 01:17:01 -0500

    Configuración Inicial del Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:' ', redirectTo:'', pathMatch:'full'
+  },
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'nosotros',
+    loadChildren: () => import('./pages/about/about.module').then(mod => mod.AboutModule)
+  },
+  {
+    path:'contacto',
+    loadChildren: () => import('./pages/contact/contact.module').then(mod => mod.ContactModule)
+  },
+  {
+    path:'servicios',
+    loadChildren: () => import('./pages/services/services.module').then(mod => mod.ServicesModule)
+  },
+  {
+    path:'capacitaciones',
+    loadChildren: () => import('./pages/training/training.module').then(mod => mod.TrainingModule)
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { BannerComponent } from './shared/components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
