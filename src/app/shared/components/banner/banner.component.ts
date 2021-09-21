2e452336c25b028446d42d29b78467d9695b65be
Junior Carrion <juniorcr2198@gmail.com>
2021-09-21 01:43:37 -0500

    Banner al 90%

--- src/app/shared/components/banner/banner.component.ts
+++ src/app/shared/components/banner/banner.component.ts
@@ -7,16 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
   title = '';
+  banner = '';
   rutas = {
     '/servicios': 'Servicios',
     '/nosotros': 'Nosotros',
     '/capacitaciones': 'Capacitaciones',
     '/contacto': 'Contacto',
   };
+  imgs = {
+    '/servicios': 'banner bannerServices',
+    '/nosotros': 'banner bannerAbout',
+    '/capacitaciones': 'banner bannerTraining',
+    '/contacto': 'banner bannerContact',
+  }
 
   constructor() {}
 
   ngOnInit(): void {
     this.title = this.rutas[location.pathname];
+    this.banner = this.imgs[location.pathname];
   }
 }
